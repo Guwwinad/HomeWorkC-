519a2f03a484c2c3cc331f3161e01d5ebd6e97eb
Darya <guwinad@yandex.ru>
2023-04-03 15:03:00 +0300

    Done Trans

---
+++ Transponirovanie/Program.cs
@@ -0,0 +1,64 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+        }
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} \t");
+
+        }
+        System.Console.WriteLine();
+    }
+}
+void Trans(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0) / 2; i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int x = matrix[i, j];
+            matrix[i, j] = matrix[i + 2, j];
+            matrix[i + 2, j] = x;
+        }
+
+    }
+}
+void Trans2(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0) / 2; i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int x = matrix[i, j];
+            matrix[i, j] = matrix[i + 1, j];
+            matrix[i + 1, j] = x;
+        }
+
+    }
+}
+Console.Clear();
+System.Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+if (size[0] == 2)
+{
+    Trans2(matrix);
+}
+else
+{
+    Trans(matrix);
+}
+System.Console.WriteLine();
+PrintMatrix(matrix);
